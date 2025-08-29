39071febe06cbd899e178a7b5baa486e3e307262
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-08-29 15:36:47 +0800

    share: n/a

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import "./styles/App.css";
-import { HelloWorld } from "./components/HelloWorld";
+import { Link } from "react-router-dom";
 
-export function App() {
-  return <HelloWorld name={"World"}></HelloWorld>;
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to='profile'>Profile Page</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/App.css";
-import App from "./App.jsx";
+import routes from "./routes";
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.querySelector("main")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
